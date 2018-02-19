843a288945e59055ca50d5858954cd29ef164a31
Troy Reeves <troyreeves2@gmail.com>
2018-02-19 14:32:23 -0600

    Commit for Chapter 8- List and its challenges

---
+++ app/src/main/java/com/cidm4385/troyr/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package com.cidm4385.troyr.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by troyr on 2/19/2018.
+ */
+
+public class CrimeListFragment extends Fragment {
+    //Nothing yet
+}
